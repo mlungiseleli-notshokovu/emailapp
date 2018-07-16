d3bbd1f42889ccdb02c5abbc1c690be2a2e5104f
mlungiseleli-notshokovu <mlungiselelinotshokovu@gmail.com>
2018-07-16 19:13:42 +0200

    initial commit

---
+++ Email.java
@@ -0,0 +1,27 @@
+package emailapp;
+
+public class Email {
+	
+	private String firstName, lastName, password, department, alternateEmail;
+
+	private int mailboxCapacity;
+	
+	//Constructor to receive the firstname and lastname
+	public Email(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	//Ask for the department
+	
+	//Generate a random password
+	
+	//Set the mailbox capacity
+	
+	//Change the password
+	
+	//Set the alternate email
+	
+	//Get methods
+
+}
---
+++ EmailApp.java
@@ -0,0 +1,10 @@
+package emailapp;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
